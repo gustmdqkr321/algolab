415684ba0be8108930c7c57c4044224fb599d46c
gustmdqkr321 <mbc3940@kookmin.ac.kr>
2023-09-23 14:00:57 +0900

    Recursion

--- recursion/fast_fibo.py
+++ recursion/fast_fibo.py
@@ -6,15 +6,15 @@ def fibo(n, arr):
         arr = divide(n,arr)
     return arr[0][1]
 
-def divide(n, arr):
-    if(n > 1):
-        arr = divide(n//2, arr)
-        arr = multi(arr, arr)
-        if(n % 2 == 1):
+def divide(n, arr): # 행렬을 2개의 행렬의 곱으로 계속 나누기 위함
+    if(n > 1): # 지수가 1이 될때까지 나눔
+        arr = divide(n//2, arr) # 지수가 짝수면 반반으로 나누고
+        arr = multi(arr, arr) # 나눈 2개를 곱해서 동일해짐
+        if(n % 2 == 1): # 짝수면 위에서 나누고 곱한 값에 기본 행렬을 곱함
             arr_base = [[1,1],[1,0]]
             arr  = multi(arr,arr_base)
     return arr       
-def multi(arr,arr2):
+def multi(arr,arr2): # 행렬 곱하는 함수
     mul_arr = [[0,0],[0,0]]
     mul_arr[0][0] = ((arr[0][0] * arr2[0][0]) + (arr[0][1] * arr2[1][0])) % 1000
     mul_arr[0][1] = ((arr[0][0] * arr2[0][1]) + (arr[0][1] * arr2[1][1])) % 1000
