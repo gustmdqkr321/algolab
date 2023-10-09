fbc2067d6c5cb5bf91ca863a79473ca46f8395df
gustmdqkr321 <mbc3940@kookmin.ac.kr>
2023-10-09 17:03:10 +0900

    test

--- work/test.py
+++ work/test.py
@@ -1,7 +1,8 @@
 from matplotlib import pyplot as plt
-
-a = [1,2,3]
-b = [1,2,3]
-
-plt.plot(a,b)
+years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
+gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
+plt.plot(years, gdp, color='green', marker='o', linestyle='solid')
+plt.title("Nominal GDP")
+plt.ylabel("Billions of $")
+plt.xlabel("Years")
 plt.show()
