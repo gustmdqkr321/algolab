0b203381404275bc0cf31b8de4a652feac4b12ce
gustmdqkr321 <mbc3940@kookmin.ac.kr>
2023-11-13 15:53:35 +0900

    폴더명 변경

--- work/test.cpp
+++ work/test.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+#define MAX 11
+
+int LCS(char* a,char* b, int m, int n)
+{
+    if(m == 0 || n == 0)
+    {
+        return 0;
+    }
+    else if(a[m-1] == b[n-1])
+    {
+        return LCS(a,b,m-1,n-1) + 1;
+    }
+    else
+    {
+        return max(LCS(a,b,m,n-1), LCS(a,b,m-1,n));
+    }
+}
 
 int main(){
     int t;
-    cin >> t; //테스트 케이스 개수 입력
-    int n;
-    for (int i=0; i<t; i++)
+    cin >> t;
+    for(int i=0; i<t; i++)
     {
-        cin >> n;
-        int arr[101];
-        for(int j=0; j<n; j++)
+        char a[MAX];
+        char b[MAX];
+        int lena = 0;
+        int lenb = 0;
+        cin >> a >> b;
+        while(a[lena] != '\0') 
         {
-            cin >> arr[j];
-        } 
-    } // test
-
-
-	return 0;
+            lena++;
+        }
+        while(b[lenb] != '\0') 
+        {
+            lenb++;
+        }
+        cout << LCS(a,b,lena,lenb) << endl;
+    }
+    return 0;
 }
