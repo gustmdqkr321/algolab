cbb83799c863e98b02aee466a9c3c3ddd2f7a309
gustmdqkr321 <mbc3940@kookmin.ac.kr>
2023-11-27 19:15:36 +0900

    DFS_stack

---
+++ work/DFS_stack.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+const int MAX_N = 1001;
+
+int adj_list[MAX_N][MAX_N];
+bool visited[MAX_N];
+
+void dfs(int start, int n, int component[], int& component_size) {
+    stack<int> s;
+    s.push(start);
+    visited[start] = true;
+    component[component_size++] = start;
+
+    while (!s.empty()) {
+        int current_node = s.top();
+        s.pop();
+
+        for (int neighbor = 1; neighbor <= n; ++neighbor) {
+            if (adj_list[current_node][neighbor] && !visited[neighbor]) {
+                s.push(neighbor);
+                visited[neighbor] = true;
+                component[component_size++] = neighbor;
+            }
+        }
+    }
+}
+
+void bubble_sort(int arr[], int n) {
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n-i; ++j) {
+            if (arr[j] < arr[j - 1]) {
+                swap(arr[j], arr[j - 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    for (int test_case = 0; test_case < t; ++test_case) {
+        int n;
+        cin >> n;
+
+        for (int i = 1; i <= n; ++i) {
+            visited[i] = false;
+            for (int j = 1; j <= n; ++j) {
+                adj_list[i][j] = 0;
+            }
+        }
+
+        for (int i = 1; i <= n; ++i) {
+            int k, m;
+            cin >> k >> m;
+
+            for (int j = 0; j < m; ++j) {
+                int v;
+                cin >> v;
+                adj_list[k][v] = 1;
+                adj_list[v][k] = 1;
+            }
+        }
+
+        int connected_component_sizes[MAX_N] = {0};
+        int component_count = 0;
+
+        for (int i = 1; i <= n; ++i) {
+            if (!visited[i]) {
+                int component[MAX_N] = {0};
+                int component_size = 0;
+                dfs(i, n, component, component_size);
+                connected_component_sizes[component_count++] = component_size;
+            }
+        }
+
+        // Bubble Sort
+        bubble_sort(connected_component_sizes, component_count);
+
+        // 출력
+        cout << component_count << " ";
+        for (int i = 0; i < component_count; ++i) {
+            cout << connected_component_sizes[i] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
